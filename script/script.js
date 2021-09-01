c45a915dc46bbbc8c129d8296ccc7054e6a9e2f9
Vinicius Daher <vinicius.daher0@gmail.com>
2021-09-01 14:27:46 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -25,55 +25,10 @@ function startGame(){
         cards = createCardsFromTechs(techs);
         shuffleCards(cards);
         
-        initializeCards(cards)
+       console.log(cards)
         
-    }
-
-
- function initializeCards(cards){
-     let gameBoard = document.getElementById("gameBoard");
-     
-     cards.forEach(card => {
-
-        let cardElement = document.createElement('div');
-        cardElement.id = card.id;
-        cardElement.classList.add(CARD);
-        cardElement.dataset.icon = card.icon;
-
-        createCardContent(card, cardElement)
-
-
-        cardElement.addEventListener("click", clickCard);
-        gameBoard.appendChild(cardElement);
-
-
-     })
-}   
-
-
-function createCardContent(card, cardElement){
-
-    createCardFace(FRONT, card, cardElement);
-    createCardFace(BACK, card, cardElement);
-
 }
 
-
-function createCardFace(face, card, element){
-
-    let cardElementFace = document.createElement('div')
-    cardElementFace.classList.add(face);
-
-    if (face == FRONT){
-        let iconElement = document.createElement('img');
-        iconElement.classList.add(ICON);
-        iconElement.src = "./img/" + card.icon + ".svg";
-        cardElementFace.appendChild(iconElement);
-    }else{
-        cardElementFace.innerHTML = "&lt/&gt";
-    }
-    element.appendChild(cardElementFace);
-}
 
 function shuffleCards(cards){
 
@@ -119,8 +74,4 @@ function shuffleCards(cards){
 
  function createId(tech){
      return tech + parseInt(Math.random() *1000);
- }
-
- function clickCard(){
-
  }
